3555bd8e3db5121f367202ae20f560a4b9b092ca
juanjosbg <juanjoseborrero95@gmail.com>
2021-11-05 21:31:07 -0500

    ready the page

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,13 @@
 // alert('pagina linkeada');
+
+// btn.js
+$(document).ready(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 40) {
+            $("#btn-up").fadeIn();
+        } else {
+            $("#btn-up").fadeOut();
+        }
+    });
+});
